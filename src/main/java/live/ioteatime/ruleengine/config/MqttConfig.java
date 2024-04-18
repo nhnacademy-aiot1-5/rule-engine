24d40116f1cf598921c8b2b1e460f305d67ae5c3
Jeongyong Lee <hua9378@gmail.com>
2024-04-18 17:23:59 +0900

    fix: 토픽 여러 개 받도록 수정

--- src/main/java/live/ioteatime/ruleengine/config/MqttConfig.java
+++ src/main/java/live/ioteatime/ruleengine/config/MqttConfig.java
@@ -13,16 +13,27 @@ import org.springframework.context.annotation.Configuration;
 public class MqttConfig {
 
     @Bean
-    public MqttClient mqttClient(MqttClientProperties mqttClientProperties,
+    public MqttClient mqttClient(
+        MqttClientProperties mqttClientProperties,
         MqttCallback mqttCallback) throws MqttException {
+
         MqttClient mqttClient = new MqttClient(mqttClientProperties.getUri(), mqttClientProperties.getId());
         mqttClient.setCallback(mqttCallback);
-
         mqttClient.connect();
         log.info("MqttClient connect success: {}", mqttClientProperties.getUri());
-        mqttClient.subscribe(mqttClientProperties.getTopic());
-        log.info("MqttClient subscribe success: {}", mqttClientProperties.getTopic());
-
+        mqttClientProperties.getTopics()
+                            .forEach(topic -> subscribeTopic(topic, mqttClient));
+        
         return mqttClient;
+    }
+
+    private void subscribeTopic(String topic, MqttClient mqttClient) {
+        try {
+            mqttClient.subscribe(topic);
+            log.info("MqttClient subscribe success: {}", topic);
+
+        } catch (MqttException ignore) {
+            log.warn("MqttClient subscribe fail: {}", topic);
+        }
     }
 }
