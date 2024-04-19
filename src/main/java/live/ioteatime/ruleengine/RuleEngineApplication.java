c32306a0b6a2c2093b01c817a79bf2e83bd056d5
dongmin <ldm030475@gmail.com>
2024-04-19 17:19:13 +0900

    fix: maven.yml 수정
    delete: MqttClientProperties.java 제거

--- src/main/java/live/ioteatime/ruleengine/RuleEngineApplication.java
+++ src/main/java/live/ioteatime/ruleengine/RuleEngineApplication.java
@@ -1,7 +1,6 @@
 package live.ioteatime.ruleengine;
 
 import live.ioteatime.ruleengine.properties.InfluxDBProperties;
-import live.ioteatime.ruleengine.properties.MqttClientProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
