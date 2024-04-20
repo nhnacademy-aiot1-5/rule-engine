bf4e36477506b9580bf2543e792ada383285afdd
dongmin <ldm030475@gmail.com>
2024-04-20 18:10:06 +0900

    fix: secrets 직접 주입

--- src/main/java/live/ioteatime/ruleengine/properties/RabbitmqInfo.java
+++ src/main/java/live/ioteatime/ruleengine/properties/RabbitmqInfo.java
@@ -1,15 +1,12 @@
 package live.ioteatime.ruleengine.properties;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@Getter
-@Setter
-public class RabbitmqProperties {
+@Data
+public class RabbitmqInfo {
     @Value("${rabbitmq.host}")
     private String host;
     @Value("${rabbitmq.port}")
