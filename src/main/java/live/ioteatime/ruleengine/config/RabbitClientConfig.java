2a6952a1784aad788591d88feea75252ec89ebd3
dongmin <ldm030475@gmail.com>
2024-04-20 18:19:18 +0900

    fix: 관리법 변경

--- src/main/java/live/ioteatime/ruleengine/config/RabbitClientConfig.java
+++ src/main/java/live/ioteatime/ruleengine/config/RabbitClientConfig.java
@@ -1,22 +1,29 @@
 package live.ioteatime.ruleengine.config;
 
 import com.rabbitmq.client.ConnectionFactory;
-import live.ioteatime.ruleengine.properties.RabbitmqInfo;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Getter
 public class RabbitClientConfig {
-
+    @Value("${rabbitmq.host}")
+    private String host;
+    @Value("${rabbitmq.port}")
+    private String port;
+    @Value("${rabbitmq.username}")
+    private String username;
+    @Value("${rabbitmq.password}")
+    private String password;
     @Bean
-    public ConnectionFactory connectionFactory(RabbitmqInfo rabbitmqInfo) {
+    public ConnectionFactory connectionFactory() {
         ConnectionFactory connectionFactory = new ConnectionFactory();
-        connectionFactory.setHost(rabbitmqInfo.getHost());
-        connectionFactory.setUsername(rabbitmqInfo.getUsername());
-        connectionFactory.setPassword(rabbitmqInfo.getPassword());
-        connectionFactory.setPort(Integer.parseInt(rabbitmqInfo.getPort()));
+        connectionFactory.setHost(host);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        connectionFactory.setPort(Integer.parseInt(port));
 
         return connectionFactory;
     }
