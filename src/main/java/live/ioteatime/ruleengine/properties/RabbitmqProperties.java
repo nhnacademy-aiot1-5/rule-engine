a6fab62b03b020323750b4165902cbeef0b1a195
dongmin <ldm030475@gmail.com>
2024-04-20 17:16:41 +0900

    fix: 관리법 변경

--- src/main/java/live/ioteatime/ruleengine/properties/RabbitmqProperties.java
+++ src/main/java/live/ioteatime/ruleengine/properties/RabbitmqProperties.java
@@ -3,7 +3,9 @@ package live.ioteatime.ruleengine.properties;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 @Getter
 @Setter
 public class RabbitmqProperties {
