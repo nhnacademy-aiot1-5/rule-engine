5b7751962530ae667adc64e40ec4fb27ab44975b
dongmin <ldm030475@gmail.com>
2024-04-20 18:12:08 +0900

    fix: secrets 직접 주입

--- src/main/java/live/ioteatime/ruleengine/RuleEngineApplication.java
+++ src/main/java/live/ioteatime/ruleengine/RuleEngineApplication.java
@@ -1,13 +1,12 @@
 package live.ioteatime.ruleengine;
 
 import live.ioteatime.ruleengine.properties.InfluxDBProperties;
-import live.ioteatime.ruleengine.properties.RabbitmqInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({InfluxDBProperties.class, RabbitmqInfo.class})
+@EnableConfigurationProperties({InfluxDBProperties.class})
 public class RuleEngineApplication {
 
     public static void main(String[] args) {
