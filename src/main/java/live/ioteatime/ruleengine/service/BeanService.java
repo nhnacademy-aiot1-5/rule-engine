42bca7a9b00a66c4ad731dc8a3633fa1a78e0ab6
dongmin <ldm030475@gmail.com>
2024-04-22 22:16:59 +0900

    modify: BeanService 반환값 교체
    add: controller 작업중, JschService 추가

--- src/main/java/live/ioteatime/ruleengine/service/BeanService.java
+++ src/main/java/live/ioteatime/ruleengine/service/BeanService.java
@@ -2,9 +2,7 @@ package live.ioteatime.ruleengine.service;
 
 import live.ioteatime.ruleengine.domain.BeanSet;
 
-import java.io.File;
-
 public interface BeanService {
 
-    File createConfig(BeanSet beanSet,String path);
+    String createConfig(BeanSet beanSet,String path);
 }
---
+++ src/main/java/live/ioteatime/ruleengine/service/JschService.java
@@ -0,0 +1,6 @@
+package live.ioteatime.ruleengine.service;
+
+public interface JschService {
+
+    void scpFile(String src, String dest);
+}
