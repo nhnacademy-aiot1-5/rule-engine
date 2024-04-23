af93d263e160d6d64102a987200515c15056219a
dongmin <ldm030475@gmail.com>
2024-04-23 20:42:24 +0900

    fix: 리펙토링

--- src/main/java/live/ioteatime/ruleengine/service/impl/JschServiceImpl.java
+++ src/main/java/live/ioteatime/ruleengine/service/impl/JschServiceImpl.java
@@ -2,6 +2,7 @@ package live.ioteatime.ruleengine.service.impl;
 
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpException;
 import live.ioteatime.ruleengine.properties.JschProperties;
@@ -18,59 +19,80 @@ import java.util.Objects;
 @Slf4j
 public class JschServiceImpl implements JschService {
     private final JschProperties jschProperties;
-    private ChannelSftp channel;
-    private Session session;
+
     @Override
     public void scpFile(String folderPath,String fileName) {
         String newDirectory = jschProperties.getSavePath()+"/"+fileName;
-        try {
+        try{
             JSch jSch = new JSch();
-            jSch.addIdentity(jschProperties.getKeyPath());
-            session = jSch.getSession(jschProperties.getUser(), jschProperties.getHost(), 22);
-            session.setConfig("StrictHostKeyChecking","no");
-            session.connect();
-            log.info("session connect {} ",session.getUserInfo());
-             channel = (ChannelSftp) session.openChannel("sftp");
-            channel.connect();
+            jSch.addIdentity(jschProperties.getPrivateKey());
+            Session session = createSession(jSch);
 
-                channel.mkdir(newDirectory);
-                putInstance(folderPath, channel, newDirectory);
-            deleteFolder(new File(folderPath));
-            channel.disconnect();
-            session.disconnect();
+            ChannelSftp channel = createChannel(session);
 
-        }catch (SftpException e){
             putInstance(folderPath, channel, newDirectory);
+            deleteFolder(folderPath);
+
             channel.disconnect();
             session.disconnect();
+
         } catch (Exception e) {
             log.error("scpFile  {} ", e.getMessage());
         }
     }
 
+    private Session createSession(JSch jSch) throws JSchException {
+        Session session = jSch.getSession(jschProperties.getUser(), jschProperties.getHost(), 22);
+        session.setConfig("StrictHostKeyChecking","no");
+        session.connect();
+        log.info("session connect {} ",session.getUserInfo());
+        return session;
+
+    }
+    private ChannelSftp createChannel(Session session) throws JSchException {
+        ChannelSftp  channel = (ChannelSftp) session.openChannel("sftp");
+        channel.connect();
+        log.info("Channel connected.");
+        return channel;
+    }
+
     private static void putInstance(String folderPath, ChannelSftp channel, String newDirectory) {
         File folder = new File(folderPath);
-        for (File file : Objects.requireNonNull(folder.listFiles())) {
+        try {
+            channel.mkdir(newDirectory);
+            uploadInstance(folder.listFiles(),newDirectory,channel);
+        } catch (SftpException e) {
+            log.info("이미 있는 설정 파일입니다. 덮어 씌우기");
+            uploadInstance(folder.listFiles(),newDirectory,channel);
+        }
+    }
+    private static void uploadInstance(File[] files,String folderPath,ChannelSftp channel ){
+        for (File file : Objects.requireNonNull(files)) {
             if (file.isFile()) {
                 try {
-                    channel.put(file.getAbsolutePath(), newDirectory);
+                    channel.put(file.getAbsolutePath(), folderPath);
+                    log.info("upload {}", file.getAbsolutePath());
                 } catch (SftpException e) {
                     e.getStackTrace();
                 }
             }
         }
     }
-    private static void deleteFolder(File folder) {
-
+    private static void deleteFolder(String folderPath) {
+        File folder = new File(folderPath);
         if (folder.isDirectory()) {
             File[] files = folder.listFiles();
             if (files != null) {
                 for (File file : files) {
-                    deleteFolder(file);
+                    deleteFolder(file.getAbsolutePath());
                 }
             }
         }
-        folder.delete();
+        if (folder.delete()) {
         log.info("delete folder {}", folder.getAbsolutePath());
+        }
+        else {
+            log.warn("Failed to delete folder {}", folder.getAbsolutePath());
+        }
     }
 }
