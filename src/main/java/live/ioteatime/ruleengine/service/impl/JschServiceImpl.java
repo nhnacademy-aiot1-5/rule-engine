39815acbebe2a722ca48098f9f21fa9364f0506f
dongmin <ldm030475@gmail.com>
2024-04-25 20:11:32 +0900

    fix: JSchManager 로 변경

--- src/main/java/live/ioteatime/ruleengine/service/impl/JschServiceImpl.java
+++ src/main/java/live/ioteatime/ruleengine/service/impl/JschServiceImpl.java
@@ -3,32 +3,42 @@ package live.ioteatime.ruleengine.service.impl;
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.SftpException;
+import live.ioteatime.ruleengine.manager.JSchManager;
 import live.ioteatime.ruleengine.properties.JschProperties;
 import live.ioteatime.ruleengine.service.JschService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class JschServiceImpl implements JschService {
     private final JschProperties jschProperties;
-    private final ChannelSftp channel;
-    private final ChannelExec channelExec;
+    private final JSchManager jSchManager;
+
+    public JschServiceImpl(JschProperties jschProperties, JSchManager jSchManager) {
+        this.jschProperties = jschProperties;
+        this.jSchManager = jSchManager;
+        jSchManager.setJSch();
+    }
 
     /**
      * @param filePath 저장할 인스턴스의 디레토리 주소
      * @param fileName 저장할 인스턴스의 디렉토리의 이름
      *                 저장할 인스턴스에 scp 보내는 메서드
+     *
      */
     @Override
     public void scpFile(String filePath, String fileName) {
-        String newDirectory = jschProperties.getSavePath() + "/" + fileName;
+        String destination = jschProperties.getSavePath() + "/" + fileName;
+        ChannelSftp channel = jSchManager.createChannelSftp();
+        ChannelExec channelExec = jSchManager.createChannelExec();
         try {
-            putInstance(filePath, channel, newDirectory);
+
+            putInstance(filePath, channel, destination);
 
             deleteFolder(filePath);
 
@@ -51,10 +61,10 @@ public class JschServiceImpl implements JschService {
             channel.mkdir(newDirectory);
             uploadInstance(filePath, newDirectory, channel);
         } catch (SftpException e) {
+            log.info("이미 존재하는 설정입니다. 덮어 씌우기");
             uploadInstance(filePath, newDirectory, channel);
         }
     }
-
 
     private static void uploadInstance(String filePath, String newDirectory, ChannelSftp channel) {
         try {
@@ -66,15 +76,12 @@ public class JschServiceImpl implements JschService {
     }
 
     /**
-     * @param folderPath - 삭제할 파일 경로
+     * @param filePath - 삭제할 파일 경로
      *                   작업 완료후 전송된 디렉토리, 파일을 삭제 하는 메서드
      */
-    private static void deleteFolder(String folderPath) {
-        File file = new File(folderPath);
-        if (file.isFile()) {
-            file.delete();
-            log.info("delete folder {}", file.getAbsolutePath());
-        }
+    private static void deleteFolder(String filePath) throws IOException {
+        Files.delete(Path.of(filePath));
+        log.info("deleteFile {}", filePath);
     }
 
 }
