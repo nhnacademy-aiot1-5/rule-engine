dc6a42e6acee0d291f831ec2801ec88b900f10b7
dongmin <ldm030475@gmail.com>
2024-04-27 14:18:02 +0900

    add: JSchManager 첫 빈 등록시 세션에 연결 확인

--- src/main/java/live/ioteatime/ruleengine/manager/JSchManager.java
+++ src/main/java/live/ioteatime/ruleengine/manager/JSchManager.java
@@ -1,37 +1,50 @@
 package live.ioteatime.ruleengine.manager;
 
-import com.jcraft.jsch.*;
+
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
+import live.ioteatime.ruleengine.exception.CreateJSchSessionException;
 import live.ioteatime.ruleengine.properties.JschProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class JSchManager {
     private final JschProperties jschProperties;
-    private Session session;
-    private ChannelSftp channelSftp;
-    private ChannelExec channelExec;
+
+    @PostConstruct
+    public void sessionCheck() throws CreateJSchSessionException {
+        Session session = createSession();
+        if (session == null || !session.isConnected()) {
+            log.error("session check failed");
+            return;
+        }
+        log.info("Session connected. User: {}, Host: {}", session.getUserName(), session.getHost());
+        log.info("session check success");
+        session.disconnect();
+    }
 
     /**
      * JSch 초기화 메서드
      */
-    public void setJSch()  {
+    public Session createSession() throws CreateJSchSessionException {
         try {
             JSch jSch = new JSch();
             jSch.addIdentity(jschProperties.getPrivateKey());
-            session = jSch.getSession(jschProperties.getUser(), jschProperties.getHost(), 22);
+            Session session = jSch.getSession(jschProperties.getUser(), jschProperties.getHost(), 22);
             session.setConfig("StrictHostKeyChecking", "no");
             session.connect();
-            log.info("Session connected. User: {}, Host: {}", session.getUserName(), session.getHost());
+            return session;
         } catch (JSchException e) {
-            log.error("Jsch init Failed {}", e.getMessage());
+            throw new CreateJSchSessionException("create JSch session failed",e);
         }
     }
 
@@ -39,9 +52,9 @@ public class JSchManager {
      *  scp 하기위한 인스턴스와의 연결 메소드
      * @return ChannelSftp
      */
-    public ChannelSftp createChannelSftp() {
+    public ChannelSftp createChannelSftp(Session session) {
         try {
-             channelSftp = (ChannelSftp) session.openChannel("sftp");
+            ChannelSftp channelSftp = (ChannelSftp) session.openChannel("sftp");
             channelSftp.connect();
             log.info("ChannelSftp connected.");
             return channelSftp;
@@ -55,9 +68,9 @@ public class JSchManager {
      *  인스턴스의 startup.sh 를 실행시키기위한 연결
      * @return ChannelExec
      */
-    public ChannelExec createChannelExec() {
+    public com.jcraft.jsch.ChannelExec createChannelExec(Session session) {
         try {
-            channelExec = (ChannelExec) session.openChannel("exec");
+            ChannelExec channelExec = (ChannelExec) session.openChannel("exec");
             channelExec.setPty(true);
             log.info("ChannelExec connected.");
             return channelExec;
@@ -65,19 +78,6 @@ public class JSchManager {
             log.error("createChannelExec Failed {}", e.getMessage());
             return null;
         }
-    }
-
-    /**
-     * 모든 채널 연결 해제 메소드
-     */
-    public void disconnect() {
-        if (channelSftp != null) {
-            channelSftp.disconnect();
-        }
-        if (channelExec != null) {
-            channelExec.disconnect();
-        }
-        log.info("Channel disconnected.");
     }
 
 }
