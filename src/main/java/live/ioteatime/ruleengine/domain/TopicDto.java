cd3bd94eed9c258be8546eb1e5bab240d70208df
dongmin <ldm030475@gmail.com>
2024-04-27 15:43:00 +0900

    fix: RuleConfig separation Result RuleConfig 안의 Result 클래스 분리 후 TopicDto 이름 변경

---
+++ src/main/java/live/ioteatime/ruleengine/domain/TopicDto.java
@@ -0,0 +1,16 @@
+package live.ioteatime.ruleengine.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class TopicDto {
+    private String place;
+    private String type;
+    private String phase;
+    private String description;
+
+}
