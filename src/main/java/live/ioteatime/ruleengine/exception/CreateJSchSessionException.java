6d930742872ebbaaff9849bf265f9eeeb7ecc345
dongmin <ldm030475@gmail.com>
2024-04-27 14:18:48 +0900

    add: 세션 연결 실패 exception

---
+++ src/main/java/live/ioteatime/ruleengine/exception/CreateJSchSessionException.java
@@ -0,0 +1,11 @@
+package live.ioteatime.ruleengine.exception;
+
+public class CreateJSchSessionException extends Exception {
+    public CreateJSchSessionException(String message) {
+        super(message);
+    }
+    public CreateJSchSessionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
