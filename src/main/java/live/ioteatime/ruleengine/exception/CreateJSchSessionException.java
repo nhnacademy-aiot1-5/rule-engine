5248de5aa8ceacd1bbe7199b480304fdfcd99c2c
dongmin <ldm030475@gmail.com>
2024-04-27 14:32:54 +0900

    fix: CreateJSchSessionException 안쓰는 메서드 정리

--- src/main/java/live/ioteatime/ruleengine/exception/CreateJSchSessionException.java
+++ src/main/java/live/ioteatime/ruleengine/exception/CreateJSchSessionException.java
@@ -1,9 +1,6 @@
 package live.ioteatime.ruleengine.exception;
 
 public class CreateJSchSessionException extends Exception {
-    public CreateJSchSessionException(String message) {
-        super(message);
-    }
     public CreateJSchSessionException(String message, Throwable cause) {
         super(message, cause);
     }
