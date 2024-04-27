2c8450a46668a99c322b2beea2f372b3c2ec2d0d
dongmin <ldm030475@gmail.com>
2024-04-27 15:58:16 +0900

    fix: threadLocal clear threadLocal 정리 메소드 추가

--- src/main/java/live/ioteatime/ruleengine/rule/impl/RuleChainImpl.java
+++ src/main/java/live/ioteatime/ruleengine/rule/impl/RuleChainImpl.java
@@ -4,28 +4,38 @@ import live.ioteatime.ruleengine.domain.MqttData;
 import live.ioteatime.ruleengine.rule.Rule;
 import live.ioteatime.ruleengine.rule.RuleChain;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class RuleChainImpl implements RuleChain {
     private final List<Rule> rules;
     private final ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
+
     @Override
     public void resetThreadLocal() {
         threadLocal.set(0);
+    }
 
+    @Override
+    public void clearThreadLocal() {
+        threadLocal.remove();
+        log.info("threadLocal clear");
     }
 
     @Override
     public void doProcess(MqttData mqttData) {
         Integer index = threadLocal.get();
         if (index >= rules.size()) {
+
             return;
         }
         threadLocal.set(index + 1);
         rules.get(index).doProcess(mqttData,this);
     }
+
 }
