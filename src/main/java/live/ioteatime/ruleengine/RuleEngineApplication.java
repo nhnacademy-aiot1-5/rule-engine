e41171b5a21b4e8b40a0d1f874001b0d1c85b434
dongmin <ldm030475@gmail.com>
2024-04-29 18:45:56 +0900

    fix: EnableScheduling 추가

--- src/main/java/live/ioteatime/ruleengine/RuleEngineApplication.java
+++ src/main/java/live/ioteatime/ruleengine/RuleEngineApplication.java
@@ -5,9 +5,11 @@ import live.ioteatime.ruleengine.properties.JschProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties({InfluxDBProperties.class, JschProperties.class})
+@EnableScheduling
 public class RuleEngineApplication {
 
     public static void main(String[] args) {
