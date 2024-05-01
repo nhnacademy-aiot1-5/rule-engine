95d467d5e1f8d6705e6ccd34ede5d7fe9173e80c
dongmin <ldm030475@gmail.com>
2024-05-01 15:00:25 +0900

    add: InfluxQuery 다중 쿼리 설정 여러 쿼리 설정

--- src/main/java/live/ioteatime/ruleengine/domain/InfluxQuery.java
+++ src/main/java/live/ioteatime/ruleengine/domain/InfluxQuery.java
@@ -3,8 +3,10 @@ package live.ioteatime.ruleengine.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Setter
 @Getter
 public class InfluxQuery {
-    private String query;
+    private List<String> queries;
 }
