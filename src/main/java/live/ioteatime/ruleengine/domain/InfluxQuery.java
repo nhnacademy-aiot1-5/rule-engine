639477a2e35c22aac1325ad43c0b627d19939c06
dongmin <ldm030475@gmail.com>
2024-05-02 11:01:26 +0900

    fix: InfluxQuery list 로 변경

--- src/main/java/live/ioteatime/ruleengine/domain/InfluxQuery.java
+++ src/main/java/live/ioteatime/ruleengine/domain/InfluxQuery.java
@@ -3,10 +3,11 @@ package live.ioteatime.ruleengine.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Setter
 @Getter
 public class InfluxQuery {
-    private List<String> queries;
+    private List<String> queries = new ArrayList<>();
 }
