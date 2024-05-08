33288b31af96a52add3244089fbe5d43bfdc26e9
dongmin <ldm030475@gmail.com>
2024-05-08 21:24:57 +0900

    add: InfluxQueryControllerTest 추가

--- src/test/java/live/ioteatime/ruleengine/controller/InfluxQueryControllerTest.java
+++ src/test/java/live/ioteatime/ruleengine/controller/InfluxQueryControllerTest.java
@@ -3,6 +3,7 @@ package live.ioteatime.ruleengine.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import live.ioteatime.ruleengine.domain.InfluxQuery;
 import live.ioteatime.ruleengine.domain.QueryRequest;
+import live.ioteatime.ruleengine.domain.QueryResponse;
 import live.ioteatime.ruleengine.manager.QueryManager;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,11 +14,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(InfluxQueryController.class)
@@ -30,10 +33,10 @@ class InfluxQueryControllerTest {
     InfluxQuery influxQuery;
 
     @Test
-    void setQuery() throws Exception {
+    void addQueryTest() throws Exception {
         QueryRequest queryRequest = new QueryRequest();
 
-        doNothing().when(queryManager).setUp(any());
+        doNothing().when(queryManager).addQuery(any());
 
         mockMvc.perform(post("/query")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -41,7 +44,36 @@ class InfluxQueryControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Create Query")));
 
-        verify(queryManager).setUp(any());
+        verify(queryManager).addQuery(any());
+    }
+
+    @Test
+    void getQueryTest() throws Exception {
+        QueryResponse queryResponse = QueryResponse.builder().number(0).query("test").build();
+        List<QueryResponse> queryResponseList = List.of(queryResponse);
+
+        when(queryManager.getQueries()).thenReturn(queryResponseList);
+
+        mockMvc.perform(get("/query")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].number").value(0))
+                .andExpect(jsonPath("$[0].query").value("test"));
+    }
+
+    @Test
+    void deleteQueryTest() throws Exception {
+        String remove = "test";
+
+        when(queryManager.deleteQuery(anyInt())).thenReturn(remove);
+
+        mockMvc.perform(get("/delete/{index}", 0)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("delete " + remove));
+
+        verify(queryManager).deleteQuery(anyInt());
     }
 
 }
