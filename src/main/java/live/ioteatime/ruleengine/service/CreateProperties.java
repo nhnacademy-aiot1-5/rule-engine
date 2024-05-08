9ed36226ad6adb9f34d142a7145a242efe552cef
dongmin <ldm030475@gmail.com>
2024-05-08 14:42:20 +0900

    feature: modbus 설정 파일 생성 modbus bridge 생성을 위한 설정 파일 생성

--- src/main/java/live/ioteatime/ruleengine/service/CreateProperties.java
+++ src/main/java/live/ioteatime/ruleengine/service/CreateProperties.java
@@ -1,8 +1,21 @@
 package live.ioteatime.ruleengine.service;
 
+import live.ioteatime.ruleengine.domain.ModbusInfo;
 import live.ioteatime.ruleengine.domain.MqttInfo;
 
 public interface CreateProperties {
 
+    /**
+     *  mqtt bridge 생성 메소드
+     * @param mqttInfo 브릿지 생성을 위한 데이터
+     * @return 만들어진 설정파일 경로
+     */
     String createConfig(MqttInfo mqttInfo);
+
+    /**
+     *  modbus bridge 생성 메소드
+     * @param modbus 브릿지 생성을 위한 데이터
+     * @return 먼들어진 설정파일 경로
+     */
+    String createConfig(ModbusInfo modbus);
 }
