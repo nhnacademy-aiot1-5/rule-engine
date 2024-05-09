dc80c229e28b8982f39ea0e2ef3d130d9cbd4b24
dongmin <ldm030475@gmail.com>
2024-05-09 18:06:25 +0900

    refactor: 일일 전력 리팩토링

--- src/main/java/live/ioteatime/ruleengine/scheduler/ReportScheduler.java
+++ src/main/java/live/ioteatime/ruleengine/scheduler/ReportScheduler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import live.ioteatime.ruleengine.domain.*;
 import live.ioteatime.ruleengine.service.OutlierService;
 import live.ioteatime.ruleengine.service.impl.DailyPowerServiceImpl;
+import live.ioteatime.ruleengine.service.impl.QueryServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Map;
 import java.util.Optional;
 
@@ -19,20 +21,23 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ReportScheduler {
     private final DailyPowerServiceImpl dailyPowerService;
+    private final QueryServiceImpl queryServiceImpl;
     private final OutlierService outlierService;
     private final OutlierRepo outlierRepo;
     @Value("${schedule.flag}")
     private boolean cronFlag;
+    LocalMidnightDto localMidnightDto = new LocalMidnightDto();
 
     @Scheduled(cron = "${schedule.cron1}")
     public void saveTodayPower() {
         if (cronFlag) {
-            LocalMidnightDto localMidnightDto = dailyPowerService.createMidnight();
+             createMidnight();
 
             for (int i = 0; i < dailyPowerService.getQueryCount(); i++) {
-                Map<LocalDateTime, Object> queryResult = dailyPowerService.convertInfluxDbResponseToLocalTime(dailyPowerService.getInfluxQuery(i));
+                Map<LocalDateTime, Object> queryResult = dailyPowerService.convertInfluxDbResponseToLocalTime(dailyPowerService.getQuery(i));
+                int channelId = queryServiceImpl.getChannelId(i);
                 double dailyPower = dailyPowerService.calculateDailyPower(queryResult, localMidnightDto);
-                dailyPowerService.insertMysql(localMidnightDto.getYesterday(), dailyPower);
+                dailyPowerService.insertMysql(localMidnightDto.getYesterday(), dailyPower, channelId, 0D);
             }
         }
     }
@@ -56,6 +61,19 @@ public class ReportScheduler {
                 log.info("outlier update to min {} max {}", outlierRepo.getMin(), outlierRepo.getMax());
             }
         }
+    }
+
+    public void createMidnight() {
+        LocalDateTime date = LocalDateTime.now();
+        LocalDateTime todayMidNight = date.with(LocalTime.MIN);
+        LocalDateTime yesterday = date.minusDays(1);
+        LocalDateTime yesterdayMidnight = yesterday.with(LocalTime.MIN);
+
+        log.info("Today Midnight {} ", todayMidNight);
+        log.info("Yesterday Midnight {} ", yesterdayMidnight);
+
+        localMidnightDto.setToday(todayMidNight);
+        localMidnightDto.setYesterday(yesterdayMidnight);
     }
 
 }
