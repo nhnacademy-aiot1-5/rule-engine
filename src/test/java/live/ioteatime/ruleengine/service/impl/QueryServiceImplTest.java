d659bd7cd700da79d67db3c89ca7132e64249b74
dongmin <ldm030475@gmail.com>
2024-05-10 17:34:01 +0900

    fix: QueryServiceImpl 테스트 코드 QueryServiceImplTest 테스트 코드 추가

--- src/test/java/live/ioteatime/ruleengine/service/impl/QueryServiceImplTest.java
+++ src/test/java/live/ioteatime/ruleengine/service/impl/QueryServiceImplTest.java
@@ -1,46 +1,53 @@
 package live.ioteatime.ruleengine.service.impl;
 
 import live.ioteatime.ruleengine.domain.QueryRequest;
+import live.ioteatime.ruleengine.domain.QueryResponse;
+import live.ioteatime.ruleengine.domain.Tags;
 import live.ioteatime.ruleengine.repository.ChannelsRepository;
 import live.ioteatime.ruleengine.repository.InfluxQueryRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 class QueryServiceImplTest {
     @Mock
     InfluxQueryRepository influxQueryRepository;
     @Mock
-    BufferedReader bufferedReader;
-    @Mock
-    FileReader fileReader;
-    @Mock
     ChannelsRepository channelsRepository;
     @InjectMocks
-    QueryServiceImpl service;
+    QueryServiceImpl queryService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        String queryPath = "path/to/query/file";
+        ReflectionTestUtils.setField(influxQueryRepository,"queryPath", queryPath);
+
+        queryService = new QueryServiceImpl(influxQueryRepository, channelsRepository){
+            @Override
+            protected BufferedReader createBufferedReader(String queryPath) {
+                return new BufferedReader(new StringReader("query1\nquery2\nquery3"));
+            }
+        };
+
+    }
 
     @Value("${my.query.path}")
     String queryPath;
-
-    @BeforeEach
-    void setUp() throws Exception {
-
-        MockitoAnnotations.openMocks(this);
-    }
 
     @Test
     void addQuery() {
@@ -52,22 +59,49 @@ class QueryServiceImplTest {
         queryRequest.setFn("test");
         queryRequest.setYield("test");
 
-        service.addQuery(queryRequest);
+        queryService.addQuery(queryRequest);
 
         verify(influxQueryRepository).writeQuery(anyString());
         verify(influxQueryRepository).updateQuery(queryPath);
-
     }
 
     @Test
-    void getQueries() throws IOException {
+    void getQueries() throws Exception {
+
+        List<QueryResponse> queries = queryService.getQueries();
+
+        assertEquals(3, queries.size());
     }
 
     @Test
     void deleteQuery() {
+        int index = 0;
+        String path = "asd";
+        when(influxQueryRepository.removeQuery(index)).thenReturn("test");
+        when(influxQueryRepository.getQueryPath()).thenReturn(path);
+
+        queryService.deleteQuery(index);
+
+        verify(influxQueryRepository).removeQuery(index);
+        verify(influxQueryRepository).modifyQuery();
+        verify(influxQueryRepository).updateQuery(anyString());
     }
 
     @Test
     void getChannelId() {
+        Tags tags = new Tags();
+        tags.setType("ads");
+        tags.setPhase("ads");
+        tags.setDescription("ads");
+        tags.setPlace("ads");
+        String query1 = "import \"timezone\"option location = timezone.fixed(offset: 9h) from(bucket: \"test\") |> range(start: -2d) |> filter(fn: (r) => r[\"place\"] == \"test\") |> filter(fn: (r) => r[\"type\"] == \"test\") |> filter(fn: (r) => r[\"phase\"] == \"test\") |> filter(fn: (r) => r[\"description\"] == \"test\") |> aggregateWindow(every: 1h, fn: last, createEmpty: false) |> yield(name: \"last\")\n";
+
+        when(influxQueryRepository.getQuery(1)).thenReturn(query1);
+        when(channelsRepository.findChannelIdByTags(anyString(), anyString(), anyString(), anyString())).thenReturn(1);
+
+        int channelId = queryService.getChannelId(1);
+
+        assertEquals(1,channelId);
+        verify(channelsRepository).findChannelIdByTags(anyString(), anyString(), anyString(), anyString());
     }
 }
