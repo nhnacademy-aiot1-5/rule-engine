f21f59962cd18489b099674ac7e64f7f2825afd2
DongminLee <ldm030475@naver.com>
2024-05-17 16:27:40 +0900

    chore: LocalDateTimeDto setter 제거 안쓰는 어노테이션 제거

--- src/main/java/live/ioteatime/ruleengine/domain/LocalDateTimeDto.java
+++ src/main/java/live/ioteatime/ruleengine/domain/LocalDateTimeDto.java
@@ -2,11 +2,9 @@ package live.ioteatime.ruleengine.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Setter
 @Getter
 @AllArgsConstructor
 public class LocalDateTimeDto {
