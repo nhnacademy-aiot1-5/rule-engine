530d8899ea20503e16528d9d4bdb75b786a73de7
DongminLee <ldm030475@naver.com>
2024-05-18 17:12:39 +0900

    test: 예왜처리 테스트 코드 추가

--- src/test/java/live/ioteatime/ruleengine/manager/JSchManagerTest.java
+++ src/test/java/live/ioteatime/ruleengine/manager/JSchManagerTest.java
@@ -1,26 +1,27 @@
 package live.ioteatime.ruleengine.manager;
 
 
+
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
+import live.ioteatime.ruleengine.exception.CreateChannelExecException;
+import live.ioteatime.ruleengine.exception.CreateChannelSftpException;
 import live.ioteatime.ruleengine.exception.CreateJSchSessionException;
 import live.ioteatime.ruleengine.properties.JschProperties;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
 class JSchManagerTest {
     @Mock
     JSch jsch;
@@ -36,12 +37,35 @@ class JSchManagerTest {
     JSchManager jSchManager;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws JSchException {
+        MockitoAnnotations.openMocks(this);
         ReflectionTestUtils.setField(jSchManager, "jSch", jsch);
+        ReflectionTestUtils.setField(jSchManager, "session", mockSession);
     }
 
     @Test
-    void createSession() throws JSchException, CreateJSchSessionException {
+    void sessionCheck_SuccessfulTest() throws CreateJSchSessionException, JSchException {
+        when(jsch.getSession(jschProperties.getUser(), jschProperties.getHost(), 22)).thenReturn(mockSession);
+        when(mockSession.isConnected()).thenReturn(true);
+
+        jSchManager.sessionCheck();
+
+        verify(mockSession).connect();
+        verify(mockSession).disconnect();
+    }
+
+    @Test
+    void sessionCheck_FailedConnection() throws Exception {
+        when(jsch.getSession(jschProperties.getUser(), jschProperties.getHost(), 22)).thenReturn(mockSession);
+        when(mockSession.isConnected()).thenReturn(false);
+
+        jSchManager.sessionCheck();
+
+        verify(mockSession, never()).disconnect();
+    }
+
+    @Test
+    void createSession_Successful() throws JSchException, CreateJSchSessionException {
         when(jschProperties.getPrivateKey()).thenReturn("private_key_path");
         when(jschProperties.getUser()).thenReturn("user");
         when(jschProperties.getHost()).thenReturn("host");
@@ -54,7 +78,19 @@ class JSchManagerTest {
     }
 
     @Test
-    void createChannelSftp() throws JSchException {
+    void createSession_ThrowsJSchException() throws Exception {
+        when(jschProperties.getPrivateKey()).thenReturn("private_key_path");
+        when(jschProperties.getUser()).thenReturn("user");
+        when(jschProperties.getHost()).thenReturn("host");
+        when(jsch.getSession(anyString(), anyString(), anyInt())).thenThrow(new JSchException());
+
+        assertThrows(CreateJSchSessionException.class, () -> {
+            jSchManager.createSession();
+        });
+    }
+
+    @Test
+    void createChannelSftp_SuccessfulTest() throws JSchException {
         String channelName = "sftp";
 
         when(mockSession.openChannel(channelName)).thenReturn(mockChannelSftp);
@@ -66,7 +102,18 @@ class JSchManagerTest {
     }
 
     @Test
-    void createChannelExec() throws JSchException {
+    void createChannelSftp_ThrowsJSchException() throws JSchException {
+        String channelName = "sftp";
+
+        when(mockSession.openChannel(channelName)).thenThrow(new JSchException());
+
+        assertThrows(CreateChannelSftpException.class, () -> {
+           jSchManager.createChannelSftp(mockSession);
+        });
+    }
+
+    @Test
+    void createChannelExec_SuccessfulTest() throws JSchException {
         String channelName = "exec";
 
         when(mockSession.openChannel(channelName)).thenReturn(mockChannelExec);
@@ -74,6 +121,17 @@ class JSchManagerTest {
         ChannelExec channelExec = jSchManager.createChannelExec(mockSession);
 
         assertEquals(mockChannelExec, channelExec);
+    }
+
+    @Test
+    void createChannelExec_ThrowsJSchException() throws JSchException {
+        String channelName = "exec";
+
+        when(mockSession.openChannel(channelName)).thenThrow(new JSchException());
+
+        assertThrows(CreateChannelExecException.class, () -> {
+            jSchManager.createChannelExec(mockSession);
+        });
     }
 
 }
