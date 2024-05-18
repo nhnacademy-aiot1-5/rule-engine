8c7e1cf692678f0bbc10420a3978a249c61e94e6
DongminLee <ldm030475@naver.com>
2024-05-18 17:11:24 +0900

    chore: createChannel Exception 추가 sftp, exec 각각 exception 추가

---
+++ src/main/java/live/ioteatime/ruleengine/exception/CreateChannelSftpException.java
@@ -0,0 +1,7 @@
+package live.ioteatime.ruleengine.exception;
+
+public class CreateChannelSftpException extends RuntimeException {
+    public CreateChannelSftpException(String message, Throwable cause) {
+        super(message,cause);
+    }
+}
