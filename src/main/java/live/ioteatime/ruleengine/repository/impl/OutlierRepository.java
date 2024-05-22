e4e703acac38382bcdf0dd181cb666bcfc46aa47
DongminLee <ldm030475@naver.com>
2024-05-22 22:39:46 +0900

    add: OutlierRepository clear 추가 OutlierRepository clear 추가

--- src/main/java/live/ioteatime/ruleengine/repository/impl/OutlierRepository.java
+++ src/main/java/live/ioteatime/ruleengine/repository/impl/OutlierRepository.java
@@ -1,13 +1,21 @@
 package live.ioteatime.ruleengine.repository.impl;
 
+import live.ioteatime.ruleengine.domain.MinMaxDto;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Setter
 @Getter
 @Component
 public class OutlierRepository {
-    private double min;
-    private double max;
+    Map<String, MinMaxDto> outliers = new HashMap<>();
+
+    public void clearOutlier() {
+        outliers.clear();
+    }
+
 }
