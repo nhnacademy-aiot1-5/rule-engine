203dec243cca8ed637690dfa709b8f0cf03a98d2
DongminLee <ldm030475@naver.com>
2024-05-28 20:42:13 +0900

    test: deleteBridge 테스트 코드 추가
    fix: JschService static 접근 제한자 제거

--- src/test/java/live/ioteatime/ruleengine/service/impl/JschServiceImplTest.java
+++ src/test/java/live/ioteatime/ruleengine/service/impl/JschServiceImplTest.java
@@ -14,6 +14,7 @@ import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.ByteArrayInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -34,33 +35,50 @@ class JschServiceImplTest {
     @InjectMocks
     JschServiceImpl jschService;
 
-    @BeforeEach
-    void setUp() {
-    }
+    String destination = "destination";
+    String filePath = "filePath";
+    String fileName = "test";
+    String type = "mqtt";
 
     @Test
     void scpFile() throws Exception {
-        try (MockedStatic<Files> files = Mockito.mockStatic(Files.class)){
-            String destination = "destination";
-            String filePath = "filePath";
-            String fileName = "test";
-            String type = "mqtt";
-            String command = "./startup.sh "+type+" "+ fileName;
-            String destinationPath = destination + "/"+type+"/" + fileName;
+        String destinationPath = destination + "/" + type + "/" + fileName;
+        String command = "./startup.sh " + type + " " + fileName;
 
-            when(properties.getSavePath()).thenReturn(destination);
-            when(jSchManager.createSession()).thenReturn(session);
-            when(jSchManager.createChannelSftp(session)).thenReturn(channelSftp);
-            when(jSchManager.createChannelExec(session)).thenReturn(channelExec);
+        when(properties.getSavePath()).thenReturn(destination);
+        when(jSchManager.createSession()).thenReturn(session);
+        when(jSchManager.createChannelExec(session)).thenReturn(channelExec);
+        when(jSchManager.createChannelSftp(session)).thenReturn(channelSftp);
+        when(channelExec.getInputStream()).thenReturn(new ByteArrayInputStream(command.getBytes()));
 
-            jschService.scpFile(filePath, fileName,type);
+        try (MockedStatic<Files> files = Mockito.mockStatic(Files.class)) {
+            jschService.scpFile(filePath, fileName, type);
 
             verify(channelSftp).mkdir(eq(destinationPath));
             verify(channelSftp).put(eq(filePath), eq(destinationPath), eq(ChannelSftp.OVERWRITE));
             files.verify(() -> Files.delete(eq(Path.of(filePath))));
             verify(channelExec).setCommand((command));
             verify(channelExec).connect();
+            verify(channelExec).getInputStream();
+            verify(channelExec).disconnect();
+            verify(channelSftp).disconnect();
         }
+    }
+
+    @Test
+    void deleteBridgeTest() throws Exception {
+        String command = "./stop.sh " + type + " " + fileName;
+
+        when(jSchManager.createSession()).thenReturn(session);
+        when(jSchManager.createChannelExec(session)).thenReturn(channelExec);
+        when(channelExec.getInputStream()).thenReturn(new ByteArrayInputStream(command.getBytes()));
+
+        jschService.deleteBridge(type, fileName);
+
+        verify(channelExec).setCommand(command);
+        verify(channelExec).connect();
+        verify(channelExec).getInputStream();
+        verify(channelExec).disconnect();
     }
 
 }
