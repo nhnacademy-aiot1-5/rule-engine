57d1b505753ef9c0f5fc217b0b43d99e6d3baa19
DongminLee <ldm030475@naver.com>
2024-05-28 20:58:02 +0900

    chore: TimeUtils 안쓰는 메소드 제거 createMidnight 제거

--- src/main/java/live/ioteatime/ruleengine/util/TimeUtils.java
+++ src/main/java/live/ioteatime/ruleengine/util/TimeUtils.java
@@ -1,12 +1,10 @@
 package live.ioteatime.ruleengine.util;
 
 import live.ioteatime.ruleengine.domain.LocalDateTimeDto;
-import live.ioteatime.ruleengine.domain.LocalMidnightDto;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Slf4j
@@ -14,7 +12,7 @@ import java.time.LocalTime;
 public class TimeUtils {
 
     /**
-     *  시간 앞 2자리만 분리, ex) 17:52:..:.. -> 17
+     * 시간 앞 2자리만 분리, ex) 17:52:..:.. -> 17
      *
      * @return 날짜 , 시각
      */
@@ -26,22 +24,6 @@ public class TimeUtils {
         log.info("localDate: {}", localDate);
 
         return new LocalDateTimeDto(localDate, time);
-    }
-
-    /**
-     *  당일, 전날의 자정 시간을 만드는 메소드
-     * @return 당일, 전날 자정
-     */
-    public static LocalMidnightDto createMidnight() {
-        LocalDateTime date = LocalDateTime.now();
-        LocalDateTime todayMidNight = date.with(LocalTime.MIN);
-        LocalDateTime yesterday = date.minusDays(1);
-        LocalDateTime yesterdayMidnight = yesterday.with(LocalTime.MIN);
-
-        log.info("Today Midnight {} ", todayMidNight);
-        log.info("Yesterday Midnight {} ", yesterdayMidnight);
-
-        return new LocalMidnightDto(yesterdayMidnight,todayMidNight);
     }
 
 }
