f57b0ff95c2b81839db4f79c01242b8889af7b7e
DongminLee <ldm030475@naver.com>
2024-05-28 20:42:27 +0900

    test: deleteBridge 테스트 코드 추가
    fix: JschService static 접근 제한자 제거

--- src/test/java/live/ioteatime/ruleengine/service/impl/JschServiceImplTest.java
+++ src/test/java/live/ioteatime/ruleengine/service/impl/JschServiceImplTest.java
@@ -5,7 +5,6 @@ import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.Session;
 import live.ioteatime.ruleengine.manager.JSchManager;
 import live.ioteatime.ruleengine.properties.JschProperties;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
