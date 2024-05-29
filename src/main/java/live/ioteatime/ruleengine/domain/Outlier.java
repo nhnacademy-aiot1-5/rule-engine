8a09a6f8c54ffc620dacce27f08441c342297143
DongminLee <ldm030475@naver.com>
2024-05-29 09:36:38 +0900

    fix: Outlier enum 수정 어노테이션 사용

--- src/main/java/live/ioteatime/ruleengine/domain/Outlier.java
+++ src/main/java/live/ioteatime/ruleengine/domain/Outlier.java
@@ -1,18 +1,14 @@
 package live.ioteatime.ruleengine.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum Outlier {
     LIGHT("do_1","light"),
     AC("do_2","ac");
 
     private final String id;
     private final String lowercase;
-
-    Outlier(String id,String lowercase) {
-        this.id = id;
-        this.lowercase = lowercase;
-    }
-
 }
