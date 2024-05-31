81bb846449f8871e72af5629625a6786a33d1471
DongminLee <ldm030475@naver.com>
2024-05-31 11:26:06 +0900

    add: 명세 스키마 명세에 사용되는 스키마 작성

--- src/main/java/live/ioteatime/ruleengine/domain/ModbusInfo.java
+++ src/main/java/live/ioteatime/ruleengine/domain/ModbusInfo.java
@@ -1,14 +1,21 @@
 package live.ioteatime.ruleengine.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "MODBUS 정보")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ModbusInfo {
+    @Schema(description = "MODBUS NAME",example = "modbus name")
     private String name;
+
+    @Schema(description = "MODBUS HOST",example = "local host")
     private String host;
+
+    @Schema(description = "MODBUS CHANNELS",example = "\"function-code/address/type, function-code/address/type....\"")
     private String channel;
 }
 
