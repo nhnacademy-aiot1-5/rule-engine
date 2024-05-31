1c34c36adc84f1c8c874235c29bd6055d07e78e6
DongminLee <ldm030475@naver.com>
2024-05-31 17:05:55 +0900

    chore: Exception code convention
    Exception 코드 정리

--- src/main/java/live/ioteatime/ruleengine/exception/CreateChannelExecException.java
+++ src/main/java/live/ioteatime/ruleengine/exception/CreateChannelExecException.java
@@ -1,6 +1,7 @@
 package live.ioteatime.ruleengine.exception;
 
 public class CreateChannelExecException extends RuntimeException {
+
     public CreateChannelExecException(String message, Throwable cause) {
         super(message,cause);
     }
