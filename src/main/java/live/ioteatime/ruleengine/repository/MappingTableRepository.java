5861ccc1492c340581aab84976e5f8119064c362
DongminLee <ldm030475@naver.com>
2024-06-02 22:49:08 +0900

    chore: logback 설정
    logback을 위한 디버깅 레벨, 주석 추

--- src/main/java/live/ioteatime/ruleengine/repository/MappingTableRepository.java
+++ src/main/java/live/ioteatime/ruleengine/repository/MappingTableRepository.java
@@ -10,12 +10,14 @@ public interface MappingTableRepository {
 
     /**
      *  모든 매핑테이블 조회하는 메소드
+     *  ex) [8000={description=sum, place=class_a, type=main}, 8032={description=sum, place=class_a, type=outdoor_unit_room_light}]
      * @return Set<Map.Entry<Integer, Map<String, String>>> 만들어진 맵 entry
      */
     Set<Map.Entry<Integer, Map<String, String>>> getTables();
 
     /**
      *  매핑테이블에서 원하는 태그 가져오는 메소드
+     *  ex) 8000 = {description=sum, place=class_a, type=main}
      * @param address 모드버스 address
      * @return Map<String, String> address 에 따른 매핑테이블
      */
